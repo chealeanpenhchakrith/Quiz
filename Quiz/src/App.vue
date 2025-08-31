d43729f07e58c210cd91894e54d17df7fc6ae308
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-08-31 21:52:29 +0200

    Configure tailwind css

--- Quiz/src/App.vue
+++ Quiz/src/App.vue
@@ -1,3 +1,10 @@
 <template>
+    <h1 class="text-sky-800 text-2xl text-bold">
+      🎬 Movie Quiz App
+    </h1>
+    <quizBlock />
 </template>
 
+<script setup>
+import quizBlock from './components/quizBlock.vue';
+</script>

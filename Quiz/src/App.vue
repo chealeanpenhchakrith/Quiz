3983a7d521b2b3888e2e90362e9ab6d78815a4a9
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-09-01 23:14:30 +0200

    Create the questionsBlock to fill with questions and possible answers

--- Quiz/src/App.vue
+++ Quiz/src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <quizBlock class="h-screen flex items-center justify-center"/>
+    <questionsBlock />
 </template>
 
 <script setup>
 import quizBlock from './components/quizBlock.vue';
+import questionsBlock from './components/questionsBlock.vue';
 </script>
---
+++ Quiz/src/components/questionsBlock.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-for="questions in questionsList" key="question"> 
+        <li> {{ questions }}</li>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted} from 'vue'
+import questions from '../assets/questions.json'
+
+const quizData = ref(null)
+const questionsList = ref(null)
+
+onMounted( () => {
+    quizData.value = questions
+    questionsList.value = quizData.value.questions
+})
+
+</script>

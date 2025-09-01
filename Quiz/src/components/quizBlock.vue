3f4636db2a145fe35c2b33bd5af0a5859227a1eb
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-09-01 22:40:54 +0200

    Import the questions.json file successfuly in the quizBlock component

--- Quiz/src/components/quizBlock.vue
+++ Quiz/src/components/quizBlock.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-    <h1 class="text-3xl">Questionnaire on Movies and TV Shows</h1>
+    <h1 class="text-3xl"> {{ title }}</h1>
+    <p ></p>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import questions from '../assets/questions.json'
+
+const quizData = ref(null)
+const title = ref('')
+
+onMounted(() => {
+  quizData.value = questions
+  title.value = quizData.value.title
+})
+
+</script>
